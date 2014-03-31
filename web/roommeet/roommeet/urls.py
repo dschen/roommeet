cb6ed457541e8f1697797d7f9af95df0d55a3b40
ltolias <ltolias@princeton.edu>
2014-03-31 15:10:02 -0400

    removed tutorial stuff, renamed things to make more sense

--- web/roommeet/roommeet/urls.py
+++ web/roommeet/roommeet/urls.py
@@ -1,15 +1,9 @@
-from django.conf.urls import patterns, include, url
-from roommeet.views import hello, current_datetime, hours_ahead, boot_home
+from django.conf.urls import *
 from django.contrib import admin
 admin.autodiscover()
 
 #regex to view function mapping
-urlpatterns = patterns('',
-    url(r'^hello/$', hello),
-    url(r'^time/$', current_datetime),
-    #parentheset around parts of regex pass as string arguments
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^boot/$', boot_home),
-    url(r'^$', boot_home),
+urlpatterns = patterns('roommeet.views',
+    url(r'^$', 'meet'),
     url(r'^admin/', include(admin.site.urls)),
 )
