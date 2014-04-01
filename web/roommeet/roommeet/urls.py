20f402b355c66e864fb74bbe3eb6c1610303dd1e
ltolias <ltolias@princeton.edu>
2014-04-01 12:11:42 -0400

    lots of new features

--- web/roommeet/roommeet/urls.py
+++ web/roommeet/roommeet/urls.py
@@ -5,5 +5,11 @@ admin.autodiscover()
 #regex to view function mapping
 urlpatterns = patterns('roommeet.views',
     url(r'^$', 'meet'),
+    url(r'^profile/$', 'profile'),
+    url(r'^meet/$', 'meet'),
+    url(r'^talk/$', 'talk'),
+    url(r'^/$', 'talk'),
+    url(r'^get_marks/$', 'get_marks'),
+    url(r'^user/$', 'user'),
     url(r'^admin/', include(admin.site.urls)),
 )
