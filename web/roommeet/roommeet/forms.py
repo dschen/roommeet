5d986da62f47beadee9dda547975cbc47cf3c767
ltolias <ltolias@princeton.edu>
2014-04-03 00:20:45 -0400

    getting rid of old stuff

--- web/roommeet/roommeet/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LatLonForm(forms.Form):
-    lat = forms.DecimalField(max_digits=13, decimal_places=10)
-    lon = forms.DecimalField(max_digits=13, decimal_places=10)
