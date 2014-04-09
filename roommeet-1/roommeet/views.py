64772a65b1b2b9a4a7fa136bb3a7b0fab1c36b20
mjd3 <mjd3@princeton.edu>
2014-04-09 00:10:03 -0400

    Error Validation in Profile Form

--- roommeet-1/roommeet/views.py
+++ roommeet-1/roommeet/views.py
@@ -12,7 +12,7 @@ from django.template import Context
 from django.utils.html import strip_tags
 
 import datetime
-from forms import LatLonForm
+from forms import ProfileForm
 
 from django.views.decorators.csrf import csrf_exempt
 from people.models import Person
@@ -128,26 +128,31 @@ def remove_person(request):
 def user(request):
 	currentNetid = request.user.username
 	if request.POST:
-		if '_save' in request.POST:
-			p = Person.objects.filter(netid=currentNetid)
-			if (p):
-				p1 = p[0];
-				p1.netid = currentNetid
-				p1.first_name = request.POST['firstname']
-				p1.last_name = request.POST['lastname']
-				p1.lat = request.POST['lat-s']
-				p1.lon = request.POST['lon-s']
-				p1.company = request.POST['company']
-				p1.year = (int)(request.POST['cyear'])
-				p1.save();
-			else:
-				p1 = Person(netid=currentNetid, first_name=request.POST['firstname'], 
-					last_name=request.POST['lastname'], lat=request.POST['lat-s'], 
-					lon=request.POST['lon-s'], company=request.POST['company'], year=request.POST['cyear'])
-				p1.save();
-			return HttpResponseRedirect('/')
-		elif '_cancel' in request.POST:
-			return HttpResponseRedirect('/')
+		form = ProfileForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			if '_save' in request.POST:
+				p = Person.objects.filter(netid=currentNetid)
+				if (p):
+					p1 = p[0];
+					p1.netid = currentNetid
+					p1.first_name = cd['first_name']
+					p1.last_name = cd['last_name']
+					p1.lat = request.POST['lat-s']
+					p1.lon = request.POST['lon-s']
+					p1.company = cd['company']
+					p1.year = cd['year']
+					p1.save();
+				else:
+					p1 = Person(netid=currentNetid, first_name=['first_name'], 
+						last_name=cd['last_name'], lat=request.POST['lat-s'], 
+						lon=request.POST['lon-s'], company=cd['company'], year=cd['year'])
+					p1.save();
+				return HttpResponseRedirect('/')
+			elif '_cancel' in request.POST:
+				return HttpResponseRedirect('/')
+		else:
+			form = ContactForm()
 			
 
 
