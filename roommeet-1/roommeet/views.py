81e10c85cb21cbd39eebce80dc23b57b132c0a26
ltolias <ltolias@princeton.edu>
2014-04-10 01:10:39 -0400

    maybe this will help

--- roommeet-1/roommeet/views.py
+++ roommeet-1/roommeet/views.py
@@ -39,7 +39,7 @@ def profile(request):
 	currentNetid = request.user.username
 	if request.method == 'POST':
 		pf = ProfileForm(request.POST)
-		return HttpResponse(pf.errors)
+		return HttpResponse(pf.non_field_errors)
 		pf.errors['lat_s'] = pf.error_class()
 		if pf.is_valid():
 			cd = pf.cleaned_data
