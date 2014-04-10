b18e368670dc0671d6fc536f70f8efd0fb779fe2
ltolias <ltolias@princeton.edu>
2014-04-10 00:43:27 -0400

    now fixed?

--- roommeet-1/roommeet/views.py
+++ roommeet-1/roommeet/views.py
@@ -39,7 +39,7 @@ def profile(request):
 	currentNetid = request.user.username
 	if request.method == 'POST':
 		pf = ProfileForm(request.POST)
-		pf.errors['lat_s'] = pf['lat_s'].error_class()
+		pf.errors['lat_s'] = pf.error_class()
 		if pf.is_valid():
 			
 			cd = pf.cleaned_data
