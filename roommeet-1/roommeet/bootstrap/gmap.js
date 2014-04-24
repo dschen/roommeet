d76051390dbf205810510b8007182c8715935e6e
ccweaver <ccweaver@princeton.edu>
2014-04-23 22:37:18 -0400

    Desired drop down

--- roommeet-1/roommeet/bootstrap/gmap.js
+++ roommeet-1/roommeet/bootstrap/gmap.js
@@ -179,48 +179,6 @@ function initialize()
 	var searchBox = new google.maps.places.SearchBox(
 		/** @type {HTMLInputElement} */(input));
 
-<<<<<<< HEAD
-  	// [START region_getplaces]
-  	// Listen for the event fired when the user selects an item from the
-  	// pick list. Retrieve the matching places for that item.
-  	google.maps.event.addListener(searchBox, 'places_changed', function() 
-  	{
-  		var places = searchBox.getPlaces();
-
-  		for (var i = 0, marker; marker = markers[i]; i++) {
-  			marker.setMap(null);
-  		}
-
-	    // For each place, get the icon, place name, and location.
-	    markers = [];
-	    var bounds = new google.maps.LatLngBounds();
-	    for (var i = 0, place; place = places[i]; i++) 
-	    {
-	    	var image = {
-	    		url: place.icon,
-	    		size: new google.maps.Size(71, 71),
-	    		origin: new google.maps.Point(0, 0),
-	    		anchor: new google.maps.Point(17, 34),
-	    		scaledSize: new google.maps.Size(25, 25)
-	    	};
-
-	      	// Create a marker for each place.
-	      	var marker = new google.maps.Marker(
-	      	{
-	      		map: map,
-	      		icon: image,
-	      		title: place.name,
-	      		position: place.geometry.location
-	      	});
-
-	      	markers.push(marker);
-
-	      	bounds.extend(place.geometry.location);
-	  	}
-
-  		map.fitBounds(bounds);
-  		map.setZoom(14);
-=======
 	// [START region_getplaces]
 	// Listen for the event fired when the user selects an item from the
 	// pick list. Retrieve the matching places for that item.
@@ -259,7 +217,6 @@ function initialize()
 		}
 		map.fitBounds(bounds);
 		map.setZoom(12);
->>>>>>> ltolias
 	});
 
 }
