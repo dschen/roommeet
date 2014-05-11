e7b508a9ab2977c7165e2e985b4d033e89285ce0
mjd3 <mjd3@princeton.edu>
2014-05-11 02:16:39 -0400

    another fix

--- roommeet-1/roommeet/bootstrap/gmap.js
+++ roommeet-1/roommeet/bootstrap/gmap.js
@@ -415,7 +415,7 @@ function showEditHouse(hid)
 	{
 		type: "post",
 		url: "/edit_house/",
-		data: {type:"edit", csrfmiddlewaretoken:document.getElementsByName('csrfmiddlewaretoken')[0].value},
+		data: {type:"new", csrfmiddlewaretoken:document.getElementsByName('csrfmiddlewaretoken')[0].value},
 		success: function (data) 
 		{
 			$("#edit-house-box").html(data.html);
